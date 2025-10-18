6844a34c17597278fafc5e1c329c1e678874f9b1
Sam Selikoff <sam.selikoff@gmail.com>
2025-10-17 17:53:55 -0700

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { ViewTransition } from 'react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}
       >
-        <main className="max-w-3xl mx-auto">
-          <ViewTransition>{children}</ViewTransition>
-        </main>
+        <main className="max-w-3xl mx-auto">{children}</main>
       </body>
     </html>
   );
